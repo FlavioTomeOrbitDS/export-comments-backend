e5302bd24fb14743d200fbee327185e40b2d5dd7
FlavioTomeOrbitDS <flavio.tome@orbitdatascience.com>
2023-02-14 08:02:20 -0300

    DOCKER FILE

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import io
 import urllib.parse
 from time import sleep
+import os
 
 
 app = Flask(__name__)
@@ -156,4 +157,5 @@ def hello():
     return 'hello server'
 
 if __name__ == '__main__':
-    app.run()
+    #port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0')
